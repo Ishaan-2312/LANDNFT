c1a01fa0822a53a7355fe88f0e3dcc0efe118cbc
Ishaan-2312 <ishaan.bansal2312@gmail.com>
2025-04-15 19:59:21 +0530

    Added Cors

--- src/main/java/com/PseudoNerds/LandNFT/Config/SecurityConfig.java
+++ src/main/java/com/PseudoNerds/LandNFT/Config/SecurityConfig.java
@@ -1,18 +1,14 @@
-package com.PseudoNerds.LandNFT.Security;
+package com.PseudoNerds.LandNFT.Config;
 
-import com.PseudoNerds.LandNFT.Util.JwtAuthenticationFilter;
 import com.PseudoNerds.LandNFT.Util.JwtUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
